2e2e9e4e79cc98d0429e674b2bfe2c31f0b97b6e
azazelcontramaestre01 <galaxyhhd@hotmail.com>
2023-03-14 09:47:14 -0300

    fix(nav-bar-ellipsis-fix): Fixed absolute position of element inside relative

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => (
     viewport={{once: true, amount: 0.25}}
     className={`${styles.xPaddings} relative`}
   >
-    <img src="/nav-elipse.png" alt="nav-elipse" className="absolute 2xl:right-[20%] right-0 z-[-10] hidden xl:block" />
+    <img src="/nav-elipse.png" alt="nav-elipse" className="absolute right-0 z-[-10] hidden xl:block flex-1" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8 pt-8`}>
       <img src="/logo.svg" alt="search" className="w-[100px] h-[32px] object-contain" />
       <button type="button" onClick={getEarlyAccess} className="bg-primary-600 p-4 text-white rounded-lg">Get Early Access</button>
